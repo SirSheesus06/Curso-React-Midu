859451742551c79b0d6fe3484d357ef00fa73eda
Juan Manuel Vivas <juanmcr.06@gmail.com>
2023-08-17 16:37:27 -0300

    agregadas las clases

--- 01.-Intro-React/01.-Hola-Mundo/src/App.jsx
+++ 01.-Intro-React/01.-Hola-Mundo/src/App.jsx
@@ -1,15 +1,20 @@
+import './App.css'
+
 export default function App () {
     return (
-        <article>
-            <header>
-                <img alt="Profile Pricture" src="https://pbs.twimg.com/profile_images/1279975466305191936/OuIZ6Z3S_400x400.jpg"></img>
-                <div>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img
+                    className='tw-followCard-img'
+                    alt="Profile Pricture" 
+                    src="https://pbs.twimg.com/profile_images/1279975466305191936/OuIZ6Z3S_400x400.jpg"></img>
+                <div className='tw-followCard-info'>
                     <strong>Juanma Vivas</strong>
-                    <span>@username</span>
+                    <span className='tw-followCard-infoUserName'>@username</span>
                 </div>
             </header>
             <aside>
-                <button>
+                <button className='tw-followCard-button'>
                     Seguir
                 </button>
             </aside>
