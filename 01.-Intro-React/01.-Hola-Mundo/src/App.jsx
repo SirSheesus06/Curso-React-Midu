97d36932ceec25e759a741911c8875d0bf21647d
Juan Manuel Vivas <juanmcr.06@gmail.com>
2023-08-17 16:29:12 -0300

    twitter card

---
+++ 01.-Intro-React/01.-Hola-Mundo/src/App.jsx
@@ -0,0 +1,20 @@
+export default function App () {
+    return (
+        <article>
+            <header>
+                <img alt="Profile Pricture" src="https://pbs.twimg.com/profile_images/1279975466305191936/OuIZ6Z3S_400x400.jpg"></img>
+                <div>
+                    <strong>Juanma Vivas</strong>
+                    <span>@username</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+            
+        </article>
+        
+    )
+}
