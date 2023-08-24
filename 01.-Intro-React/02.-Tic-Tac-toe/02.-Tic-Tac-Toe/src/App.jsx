5a4b67f4f059bb6aee78bc9571cd882e3da2e0a7
SirSheesus06 <toot.sheesus.ram@gmail.com>
2023-08-23 22:37:03 -0300

    update board and select turns

--- 01.-Intro-React/02.-Tic-Tac-toe/02.-Tic-Tac-Toe/src/App.jsx
+++ 01.-Intro-React/02.-Tic-Tac-toe/02.-Tic-Tac-Toe/src/App.jsx
@@ -13,8 +13,14 @@ const TURNS = {
 const Square = ({ children, isSelected, updateBoard, index}) => {
 
   const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
   return (
-    <div className={className}>
+    // Cuando el usuario hace click llama a handleClik que a su vez
+    // LLama a updateBoard para actualizar el estado
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
@@ -23,19 +29,44 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
 function App() {
 
   // Creo el tablero que renderizara el juego
-  // Tendra 9 cuadrados creados 
+  // Tendra 9 cuadrados creados decretados como un array
+  // Al cual le indico la cantidad que quiero con el metodo .fill()
   const [board, setBoard] = useState(Array(9).fill(null))
 
+  // Creo el estado inicial de los Turnos, al cual
+  // LE asigno por defecto que inicie en 'X'
   const [turn, setTurn] = useState(TURNS.X)
 
+  const updateBoard = (index) => {
+    // Primero hay que comprobar que turno es el actual y pasarlo al contrario
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    // Con esto hacemos que se elija el turno a jugar
+    // Y se lo pasamos a setTurn para que actualize el estado
+    setTurn(newTurn)
+
+  }
+
   return (
+
     <main className='board'>
       <h1>Tic-Tac-Toe</h1>
       <section className='game'>
         {
+          // Se renderiza el tablero, que dentro va a tener el componente
+          // <Square />, repetido 9 veces
           board.map((_, index) => {
             return (
-              <Square key={index} index={index}>{board[index]}</Square>
+              <Square 
+                key={index} 
+                // Con esto sabemos el indice de cada Square
+                index={index}
+                // En Vez de pasar la ejecucion de la funcion
+                // Se pasa la funcion para que se ejecute
+                // Solo cuando se hace click en un componente <Square />
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
             )
           })
         }
